be4dc7ce17fb8a4776a112aa1de387529b7e13ef
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-25 11:34:30 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Box, Container, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function App() {
+  return (
+    <Container alignItems={"center"}>
+      <Text border={"2px red solid"}>This is it</Text>
+    </Container>
+  );
+}
